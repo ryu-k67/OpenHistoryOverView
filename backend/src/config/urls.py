7a0f1505739f022aaaa3ccc98af5a1d786962f5f
ryu-k67 <o2ro8chi@gmail.com>
2023-05-05 16:00:23 +0900

    [add] simpleJWTによるユーザー認証 & カスタムユーザ

--- backend/src/config/urls.py
+++ backend/src/config/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
+    path('api/login/', TokenObtainPairView.as_view()),
+    path('api/refresh/', TokenRefreshView.as_view()),
+    path('api/verify/', TokenVerifyView.as_view()),
+    path('api/auth/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
