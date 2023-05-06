65df2f8b62d050b7fcb6428f4532633ee7930b9a
ryu-k67 <o2ro8chi@gmail.com>
2023-05-07 01:24:35 +0900

    [add] ユーザー登録

--- frontend/src/tailwind.config.js
+++ frontend/src/tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode:'jit',
+  purge:[
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
