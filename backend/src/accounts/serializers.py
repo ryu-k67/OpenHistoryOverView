b2866d6b3c550545bfecff30686669e21324de8c
ryu-k67 <71884466+ryu-k67@users.noreply.github.com>
2023-05-27 19:07:22 +0900

    login test1

--- backend/src/accounts/serializers.py
+++ backend/src/accounts/serializers.py
@@ -7,3 +7,16 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fileds = ('id', 'name', 'email')
+
+
+# from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+# #トークンを発行するためのクラス
+# class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+#     @classmethod
+#     def get_token(cls, user):
+#         token = super(MyTokenObtainPairSerializer, cls).get_token(user)
+
+#         # Add custom claims
+#         return token
--- backend/src/config/urls.py
+++ backend/src/config/urls.py
@@ -14,14 +14,37 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+# from django.contrib import admin
+# from django.urls import path, include
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-urlpatterns = [
-    path('api/login/', TokenObtainPairView.as_view()),
-    path('api/refresh/', TokenRefreshView.as_view()),
-    path('api/verify/', TokenVerifyView.as_view()),
-    path('api/auth/', include('accounts.urls')),
-    path('admin/', admin.site.urls),
+# urlpatterns = [
+#     path('api/login/', TokenObtainPairView.as_view()),
+#     path('api/refresh/', TokenRefreshView.as_view()),
+#     path('api/verify/', TokenVerifyView.as_view()),
+#     path('api/auth/', include('accounts.urls')),
+#     path('admin/', admin.site.urls),
+# ]
+
+
+# from django.contrib import admin
+# # from django.db import router
+# from django.urls import path, include
+# from rest_framework_simplejwt import views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/v1/', include('accounts.urls')),
+#     # jwt-tokenを取得
+#     path('api-auth/jwt/', views.TokenObtainPairView.as_view()),
+#     # jwt-tokenを再取得
+#     path('api-auth/jwt/refresh', views.TokenRefreshView.as_view()),
+# ]
+
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns=[
+    path('admin/',admin.site.urls),
+    path('api/',include('accounts.urls'))
 ]
