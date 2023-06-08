30b582d1590e58a43345fe98459509a062778226
ryu-k67 <71884466+ryu-k67@users.noreply.github.com>
2023-06-09 01:42:15 +0900

    [fix] APIサーバーへのアクセスURL

--- frontend/src/src/context/AuthContext.js
+++ frontend/src/src/context/AuthContext.js
@@ -33,7 +33,7 @@ export const AuthProvider=({children})=>{
         let body=JSON.stringify({'name':e.target.name.value,'email':e.target.email.value,'password':e.target.password.value})
         console.log(body)
         // let response=await fetch('http://127.20.0.4:8000/api/register/',{
-        let response=await fetch('http://django:8000/api/register/',{
+        let response=await fetch('http://localhost:8000/api/register/',{
             method:'POST',
             headers:{
                 'Content-Type':'application/json',
@@ -70,7 +70,7 @@ export const AuthProvider=({children})=>{
         e.preventDefault()
         console.log('From submitted')
         // let response=await fetch('http://127.20.0.4:8000/api/token/',{
-        let response=await fetch('http://django:8000/api/token/',{
+        let response=await fetch('http://localhost:8000/api/token/',{
             method:'POST',
             headers:{
                 'Content-Type':'application/json',
@@ -113,7 +113,7 @@ export const AuthProvider=({children})=>{
 
     let updateToken = async()=>{
         console.log('Update token')
-        let response=await fetch('http://django:8000/api/token/refresh/',{
+        let response=await fetch('http://localhost:8000/api/token/refresh/',{
             method:'POST',
             headers:{
                 'Content-Type':'application/json',
